01a84a39adc45f397bdc41a57d7bc0bcec1d4dcc
Eashir Arafat <Eas_dhk@hotmail.com>
2019-02-13 21:56:30 +0100

    Add project files.

---
+++ AdBook/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace AdBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.txtBoxStreetName = new System.Windows.Forms.TextBox();
+            this.txtBoxPostCode = new System.Windows.Forms.TextBox();
+            this.txtBoxPostName = new System.Windows.Forms.TextBox();
+            this.txtBoxTelephoneNo = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblStreetName = new System.Windows.Forms.Label();
+            this.lblPostCode = new System.Windows.Forms.Label();
+            this.lblPostName = new System.Windows.Forms.Label();
+            this.lblTelephoneNo = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtBoxSearch = new System.Windows.Forms.TextBox();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(160, 112);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(391, 20);
+            this.txtBoxName.TabIndex = 1;
+            // 
+            // txtBoxStreetName
+            // 
+            this.txtBoxStreetName.Location = new System.Drawing.Point(160, 152);
+            this.txtBoxStreetName.Name = "txtBoxStreetName";
+            this.txtBoxStreetName.Size = new System.Drawing.Size(391, 20);
+            this.txtBoxStreetName.TabIndex = 2;
+            // 
+            // txtBoxPostCode
+            // 
+            this.txtBoxPostCode.Location = new System.Drawing.Point(160, 191);
+            this.txtBoxPostCode.Name = "txtBoxPostCode";
+            this.txtBoxPostCode.Size = new System.Drawing.Size(391, 20);
+            this.txtBoxPostCode.TabIndex = 3;
+            // 
+            // txtBoxPostName
+            // 
+            this.txtBoxPostName.Location = new System.Drawing.Point(160, 228);
+            this.txtBoxPostName.Name = "txtBoxPostName";
+            this.txtBoxPostName.Size = new System.Drawing.Size(391, 20);
+            this.txtBoxPostName.TabIndex = 4;
+            // 
+            // txtBoxTelephoneNo
+            // 
+            this.txtBoxTelephoneNo.Location = new System.Drawing.Point(160, 267);
+            this.txtBoxTelephoneNo.Name = "txtBoxTelephoneNo";
+            this.txtBoxTelephoneNo.Size = new System.Drawing.Size(391, 20);
+            this.txtBoxTelephoneNo.TabIndex = 5;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(75, 115);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 14;
+            this.lblName.Text = "Name";
+            // 
+            // lblStreetName
+            // 
+            this.lblStreetName.AutoSize = true;
+            this.lblStreetName.Location = new System.Drawing.Point(75, 155);
+            this.lblStreetName.Name = "lblStreetName";
+            this.lblStreetName.Size = new System.Drawing.Size(66, 13);
+            this.lblStreetName.TabIndex = 15;
+            this.lblStreetName.Text = "Street Name";
+            // 
+            // lblPostCode
+            // 
+            this.lblPostCode.AutoSize = true;
+            this.lblPostCode.Location = new System.Drawing.Point(75, 190);
+            this.lblPostCode.Name = "lblPostCode";
+            this.lblPostCode.Size = new System.Drawing.Size(56, 13);
+            this.lblPostCode.TabIndex = 16;
+            this.lblPostCode.Text = "Post Code";
+            // 
+            // lblPostName
+            // 
+            this.lblPostName.AutoSize = true;
+            this.lblPostName.Location = new System.Drawing.Point(75, 227);
+            this.lblPostName.Name = "lblPostName";
+            this.lblPostName.Size = new System.Drawing.Size(59, 13);
+            this.lblPostName.TabIndex = 17;
+            this.lblPostName.Text = "Post Name";
+            // 
+            // lblTelephoneNo
+            // 
+            this.lblTelephoneNo.AutoSize = true;
+            this.lblTelephoneNo.Location = new System.Drawing.Point(75, 266);
+            this.lblTelephoneNo.Name = "lblTelephoneNo";
+            this.lblTelephoneNo.Size = new System.Drawing.Size(75, 13);
+            this.lblTelephoneNo.TabIndex = 18;
+            this.lblTelephoneNo.Text = "Telephone No";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnSave.Location = new System.Drawing.Point(487, 355);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnUpdate.Location = new System.Drawing.Point(319, 355);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(76, 23);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(288, 664);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnSearch.Location = new System.Drawing.Point(78, 390);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(72, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Location = new System.Drawing.Point(160, 304);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(391, 20);
+            this.txtBoxEmail.TabIndex = 6;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(75, 303);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 19;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtBoxSearch
+            // 
+            this.txtBoxSearch.Location = new System.Drawing.Point(171, 392);
+            this.txtBoxSearch.Name = "txtBoxSearch";
+            this.txtBoxSearch.Size = new System.Drawing.Size(415, 20);
+            this.txtBoxSearch.TabIndex = 7;
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.BackColor = System.Drawing.Color.Gray;
+            this.btnShowAll.Location = new System.Drawing.Point(12, 664);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(98, 23);
+            this.btnShowAll.TabIndex = 11;
+            this.btnShowAll.Text = "Show All";
+            this.btnShowAll.UseVisualStyleBackColor = false;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 433);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(613, 207);
+            this.listView1.TabIndex = 19;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Street Name";
+            this.columnHeader2.Width = 105;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Post Code";
+            this.columnHeader3.Width = 83;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Post Name";
+            this.columnHeader4.Width = 76;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Telephone No";
+            this.columnHeader5.Width = 96;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Email";
+            this.columnHeader6.Width = 141;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FloralWhite;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label1.Location = new System.Drawing.Point(222, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 31);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Address Book";
+            // 
+            // btnNew
+            // 
+            this.btnNew.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnNew.Location = new System.Drawing.Point(160, 355);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 21;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = false;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Moccasin;
+            this.btnExit.Location = new System.Drawing.Point(550, 664);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(637, 707);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txtBoxSearch);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTelephoneNo);
+            this.Controls.Add(this.lblPostName);
+            this.Controls.Add(this.lblPostCode);
+            this.Controls.Add(this.lblStreetName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.txtBoxTelephoneNo);
+            this.Controls.Add(this.txtBoxPostName);
+            this.Controls.Add(this.txtBoxPostCode);
+            this.Controls.Add(this.txtBoxStreetName);
+            this.Controls.Add(this.txtBoxName);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "AddressBook";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.TextBox txtBoxStreetName;
+        private System.Windows.Forms.TextBox txtBoxPostCode;
+        private System.Windows.Forms.TextBox txtBoxPostName;
+        private System.Windows.Forms.TextBox txtBoxTelephoneNo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblStreetName;
+        private System.Windows.Forms.Label lblPostCode;
+        private System.Windows.Forms.Label lblPostName;
+        private System.Windows.Forms.Label lblTelephoneNo;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtBoxSearch;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ AdBook/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AdBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const string path = @"C:\Users\....";
+        public void ClearAll()  // Tabort alla texter från textbox
+        {
+            txtBoxName.Clear();
+            txtBoxPostName.Clear();
+            txtBoxPostCode.Clear();
+            txtBoxStreetName.Clear();
+            txtBoxTelephoneNo.Clear();
+            txtBoxEmail.Clear();
+            txtBoxName.Focus();
+        }
+
+        private void btnSave_Click(object sender, System.EventArgs e)
+        {
+            string name = txtBoxName.Text;
+            string streetName = txtBoxStreetName.Text;
+            string postCode = txtBoxPostCode.Text;
+            string postName = txtBoxPostName.Text;
+            string telephoneNo = txtBoxTelephoneNo.Text;
+            string email = txtBoxEmail.Text;
+            Save(name, streetName, postCode, postName, telephoneNo, email); // Spara ny kontakter
+            ClearAll();
+        }
+
+        public void Save(string name, string streetName, string postCode, string postName, string telephoneNo, string email) // Kontorellera för att spara till .txt fil
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+
+            string missing = "";
+            if (txtBoxName.Text == "")
+            {
+                missing = "name";
+            }
+            if (txtBoxStreetName.Text == "")
+            {
+                missing = missing + ",street name";
+            }
+            if (txtBoxPostCode.Text == "")
+            {
+                missing += ",post code";
+            }
+            if (txtBoxPostName.Text == "")
+            {
+                missing += ",post address";
+            }
+            if (txtBoxTelephoneNo.Text == "")
+            {
+                missing += ",telephone no";
+            }
+            if (txtBoxEmail.Text == "")
+            {
+                missing += ",email";
+            }
+            if (missing == "")
+            {
+                sw.WriteLine(name + "," + streetName + "," + postCode + "," + postName + "," + telephoneNo + "," + email);
+                MessageBox.Show($" Tack {txtBoxName.Text}! All information sparas framgångsrikt .. ");
+            }
+            else
+            {
+                if (missing.IndexOf(",") == 0)
+                {
+                    missing = missing.Substring(1, missing.Length - 1);
+                }
+                MessageBox.Show($"Filla upp {missing} och forsök igen");
+            }
+            sw.Close();
+        }
+
+        public new List<string> Load() 
+        {
+            List<string> addressBook = new List<string>();
+            StreamReader sr = new StreamReader(path, true); 
+            string row = "";
+            while ((row = sr.ReadLine()) != null)
+            {
+                addressBook.Add(row);
+            }
+            sr.Close();
+            return addressBook;
+        }
+
+        private void btnShowAll_Click(object sender, System.EventArgs e)
+        {
+            listView1.Items.Clear();
+            List<string> infoList = Load();
+            foreach (var item in infoList)
+            {
+                string[] info = item.Split(',');
+                ListViewItem listItems = new ListViewItem(info);
+                listView1.Items.Add(listItems);
+            }
+
+        }
+
+        public new void Update()
+        {
+            try
+            {
+                if (txtBoxName.Text == "" ||
+                    txtBoxStreetName.Text == "" ||
+                    txtBoxPostCode.Text == "" ||
+                    txtBoxPostName.Text == "" ||
+                    txtBoxTelephoneNo.Text == "" ||
+                    txtBoxEmail.Text == "")
+                {
+                    MessageBox.Show("Filla up alla information for att uppdetera.");
+                }
+                else
+                {
+                    string name = txtBoxName.Text;
+                    string streetName = txtBoxStreetName.Text;
+                    string postCode = txtBoxPostCode.Text;
+                    string postName = txtBoxPostName.Text;
+                    string telephoneNo = txtBoxTelephoneNo.Text;
+                    string email = txtBoxEmail.Text;
+
+                    listView1.SelectedItems[0].SubItems[0].Text = name;
+                    listView1.SelectedItems[0].SubItems[1].Text = streetName;
+                    listView1.SelectedItems[0].SubItems[2].Text = postCode;
+                    listView1.SelectedItems[0].SubItems[3].Text = postName;
+                    listView1.SelectedItems[0].SubItems[4].Text = telephoneNo;
+                    listView1.SelectedItems[0].SubItems[5].Text = email;
+                    Save(name, streetName, postCode, postName, telephoneNo, email);
+
+                    List<string> infoList = Load();
+                    foreach (var item in infoList)
+                    {
+                        int itemIndex = 0;
+                        if (listView1.Items[itemIndex].SubItems[0].Text == listView1.Items[itemIndex].SubItems[0].Text)
+                        {
+                            File.WriteAllLines(path,
+                                listView1.Items.Cast<ListViewItem>().
+                                Select(lvItems => lvItems.Text + "," + lvItems.SubItems[1].Text
+                            + "," + lvItems.SubItems[2].Text + "," + lvItems.SubItems[3].Text + "," + lvItems.SubItems[4].Text + "," + lvItems.SubItems[5].Text));
+                        }
+                    }
+                    ClearAll();
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+            }
+        }
+
+        public void Remove()
+        {
+            try
+            {
+
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    DialogResult answer = MessageBox.Show("Är du säker att remove?", "Remove", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+
+                    if (answer == DialogResult.OK)
+                    {
+                        foreach (ListViewItem lvItems in listView1.SelectedItems.Cast<ListViewItem>())
+                        {
+                            listView1.Items.Remove(lvItems);
+
+                        }
+                        File.WriteAllLines(path,
+                            listView1.Items.Cast<ListViewItem>().
+                            Select(lvItems => lvItems.Text + "," + lvItems.SubItems[1].Text
+                        + "," + lvItems.SubItems[2].Text + "," + lvItems.SubItems[3].Text + "," + lvItems.SubItems[4].Text + "," + lvItems.SubItems[5].Text));
+                    }
+                    else if (answer == DialogResult.Cancel)
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Välj information som ska raderas.");
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // when selected as search result and trying to delete .. it gives error  || you have to select first to delete..
+            }
+        }
+
+        private void Search()  //Leta efter persons information och skriva ut matchande object i listview
+        {
+            if (txtBoxSearch.Text == "")
+            {
+                MessageBox.Show("Filla up information för att söka.");
+
+            }
+            else
+            {
+                listView1.Items.Clear();
+                List<string> infoList = Load();
+                foreach (var item in infoList)
+                {
+                    if (item.Contains(txtBoxSearch.Text))
+                    {
+                        string[] info = item.Split(',');
+                        ListViewItem listItem = new ListViewItem(info);
+                        listView1.Items.Add(listItem);
+                    }
+                }
+                txtBoxSearch.Clear();
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void btnUpdate_Click(object sender, System.EventArgs e)
+        {
+            Update();
+            ClearAll();
+        }
+
+        private void btnDelete_Click(object sender, System.EventArgs e)
+        {
+            Remove();
+            ClearAll();
+        }
+
+        private void btnSearch_Click(object sender, System.EventArgs e)
+        {
+            Search();
+
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)     //När du väljer en rad i listview visas den i textboxes
+        {
+            try
+            {
+                string name = listView1.SelectedItems[0].SubItems[0].Text;
+                string streetName = listView1.SelectedItems[0].SubItems[1].Text;
+                string postCode = listView1.SelectedItems[0].SubItems[2].Text;
+                string postName = listView1.SelectedItems[0].SubItems[3].Text;
+                string telephoneNo = listView1.SelectedItems[0].SubItems[4].Text;
+                string email = listView1.SelectedItems[0].SubItems[5].Text;
+
+                txtBoxName.Text = name;
+                txtBoxStreetName.Text = streetName;
+                txtBoxPostCode.Text = postCode;
+                txtBoxPostName.Text = postName;
+                txtBoxTelephoneNo.Text = telephoneNo;
+                txtBoxEmail.Text = email;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // När du väljer en tom rad .. kan det visa ett fel.
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
